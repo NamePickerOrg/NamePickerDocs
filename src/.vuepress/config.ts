c3d85b723b4bf68a2dab2e9e277adeac584c3bb3
LHGS_github <340994706@qq.com>
2025-03-30 15:05:32 +0800

    1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/NamePicker-docs/",
 
   lang: "zh-CN",
   title: "NamePicker Docs",
